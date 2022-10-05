7ef6e1159cb9d32152da6084681239ff6e15ff80
Desturo <47252476+Desturo@users.noreply.github.com>
2022-10-05 14:06:16 +0200

    added socket io

--- server.js
+++ server.js
@@ -1,11 +1,19 @@
 const express = require('express')
 const app = express()
-const port = process.env.PORT || 3500
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io")
+const io = new Server(server)
+const PORT = process.env.PORT || 3500
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-app.listen(port, () => {
-  console.log(`Test app listening on port ${port}`)
+io.on('connection', (socket) => {
+  console.log(`connected a new user: ${socket}`);
+})
+
+server.listen(PORT, () => {
+  console.log(`listening on port ${PORT}`);
 })
